323dfd8279cb81166efd847001b68939ccb83bc2
root <eredman@ualberta.ca>
2023-01-13 22:48:45 -0500

    init biosignal ML classifiers

--- main/Assets/GetBiosignals.cs
+++ main/Assets/GetBiosignals.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 using brainflow;
 using brainflow.math;
@@ -12,6 +13,7 @@ namespace Foundry
     {
         private BoardShim board_shim = null;
         private int sampling_rate = 0;
+        private double[] filtered;
 
         // Start is called before the first frame update
         void Start()
@@ -36,21 +38,22 @@ namespace Foundry
 
                 board_shim = new BoardShim(board_id, input_params);
                 board_shim.prepare_session();
+                
+                // board_shim.config_board("p61"); //# to enable ppg only use p61, p50 enables aux(5th eeg) channel, ppg and smth else
+
                 board_shim.start_stream(450000, "file://brainflow_data.csv:w");
                 sampling_rate = BoardShim.get_sampling_rate(board_id);
                 Debug.Log("Brainflow streaming was started");
 
-                // BoardShim.set_log_file("brainflow_log.txt");
-                // BoardShim.enable_dev_board_logger();
+                // MINDFULNESS= 0, DEAFULT MODEL = 0
+                BrainFlowModelParams concentration_params = new BrainFlowModelParams(0, 0);
+                MLModel concentration = new MLModel(concentration_params);
+                concentration.prepare();
 
-                // BrainFlowInputParams input_params = new BrainFlowInputParams();
-                // int board_id = (int)BoardIds.MUSE_2_BLED_BOARD;
-                // input_params.serial_port = "COM5";
-                // board_shim = new BoardShim(board_id, input_params);
-                // board_shim.prepare_session();
-                // board_shim.start_stream(450000, "file://brainflow_data.csv:w");
-                // sampling_rate = BoardShim.get_sampling_rate(board_id);
-                // Debug.Log("Brainflow streaming was started");
+                // RESTFULNESS= 1, DEAFULT MODEL = 0
+                BrainFlowModelParams restfulness_params = new BrainFlowModelParams(0, 0);
+                MLModel restfulness = new MLModel(restfulness_params);
+                restfulness.prepare();
             }
             catch (BrainFlowError e)
             {
@@ -66,10 +69,34 @@ namespace Foundry
             {
                 return;
             }
+
             int number_of_data_points = sampling_rate * 4;
             double[,] data = board_shim.get_current_board_data(number_of_data_points);
             // check https://brainflow.readthedocs.io/en/stable/index.html for api ref and more code samples
-            Debug.Log("Num elements: " + data.GetLength(1));
+            Debug.Log("Num eeg elements: " + data.GetLength(1));
+            Debug.Log(String.Join(",", GetRow(data,1)));
+
+            int[] ppg_channels = BoardShim.get_ppg_channels(21);
+            Debug.Log(ppg_channels);
+
+            // double[,] unprocessed_data = BoardShim.get_current_board_data(number_of_data_points);
+            // if (unprocessed_data.GetRow(0).Length < number_of_data_points)
+            // {
+            //     return; // wait for more data
+            // }
+
+            // for (int i = 0; i < eeg_channels.Length; i++)
+            // {
+            //     filtered = DataFilter.perform_wavelet_denoising (unprocessed_data.GetRow (eeg_channels[i]), "db4", 3);
+            //     // Debug.Log("channel " + eeg_channels[i] + " = " + filtered[i].ToString());
+            // }
+
+            // // prepare feature vector
+            // Tuple<double[], double[]> bands = DataFilter.get_avg_band_powers (unprocessed_data, eeg_channels, sampling_rate, true);
+            
+            // double[] feature_vector = bands.Item1.Concatenate (bands.Item2);
+            // Debug.Log("Concentration: " + concentration.predict (feature_vector)); // calc and print concetration level
+            
         }
 
         // you need to call release_session and ensure that all resources correctly released
@@ -88,5 +115,19 @@ namespace Foundry
                 Debug.Log("Brainflow streaming was stopped");
             }
         }
+
+        private T[] GetRow<T>(T[,] matrix, int rowNumber)
+        {
+            return Enumerable.Range(0, matrix.GetLength(1))
+                    .Select(x => {
+                        // Debug.Log(matrix[rowNumber, x]);
+                        // return matrix[rowNumber, x];
+                        return matrix[x, rowNumber];
+                    })
+                    .ToArray();
+        }
+
+        
+        
     }
 }
