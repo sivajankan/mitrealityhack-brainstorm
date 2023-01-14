e8d6846f47d1e5dbcb45b0280117869fe344e1d4
Ringo <ringo_kam@hotmail.com>
2023-01-14 12:43:56 -0500

    update VR player with sync values (#3)
    * init
    
    * boolean flag for button
    
    * updated multi player
    
    * settings
    
    * specify dev region to us
    
    * server changes

---
+++ main/Assets/_scripts/PlayerMindfullness.cs
@@ -0,0 +1,59 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+namespace Foundry
+{
+    public class PlayerMindfullness : MonoBehaviourPunCallbacks, IPunObservable
+    {
+
+        // FOR DEBUG PURPOSE
+        public InputActionReference incrementButton;
+        public InputActionReference decrementButton;
+        public float deltaRange = 0.1f;
+        public UnityEvent<string> onValueChange;
+
+        public float mindfullness = 0f;
+
+        void Start()
+        {
+            this.photonView.ObservedComponents.Add(this);
+
+            incrementButton.action.performed += Increment;
+            decrementButton.action.performed += Decrement;
+        }
+
+        public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+        {
+            if (stream.IsWriting)
+            {
+                stream.SendNext(mindfullness);
+            }
+            else
+            {
+                mindfullness = (float)stream.ReceiveNext();
+            }
+        }
+
+        public void Increment(InputAction.CallbackContext ctx)
+        {
+            Debug.Log("increase");
+            var val = mindfullness + deltaRange;
+      
+            mindfullness = val;
+            onValueChange.Invoke(mindfullness.ToString());
+        }
+
+        public void Decrement(InputAction.CallbackContext ctx)
+        {
+            Debug.Log("increase");
+            var val = mindfullness - deltaRange;
+
+            mindfullness = val;
+            onValueChange.Invoke(mindfullness.ToString());
+        }
+    }
+}
