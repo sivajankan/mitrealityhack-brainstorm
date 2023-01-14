f044c27bc0e7b7dc9c1a3834e9ab716c9604f6cc
Siva Kandasamy <sivajankan@gmail.com>
2023-01-14 13:55:40 -0500

    animal move with bounce - level 2

--- main/Assets/Scripts/AnimalMove.cs
+++ main/Assets/Scripts/AnimalMove.cs
@@ -6,16 +6,27 @@ namespace Foundry
 {
     public class AnimalMove : MonoBehaviour
     {
+        public GameObject animal;
+        public int movingSpeed;
+        private float speed;
+
         // Start is called before the first frame update
         void Start()
         {
-        
+            speed = movingSpeed / 1000f;
         }
 
         // Update is called once per frame
         void Update()
         {
-        
+            loop();
+        }
+
+        void loop()
+        {
+            float xPos = transform.position.x + speed;
+            float yPos = transform.position.y + speed;
+            transform.position = new Vector3(xPos, yPos, transform.position.z);
         }
     }
 }
