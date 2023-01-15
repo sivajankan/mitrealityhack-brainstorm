6db6775b9778119c00ac1a1c7aea508218f047ad
Ringo <ringo_kam@hotmail.com>
2023-01-15 10:06:38 -0500

    Staff laser go brrr (#5)
    * laser go br part 1
    
    * shoot laser brrr part 2
    
    * Add mindfulness collisions to staff/animal collisions
    
    * Fix animal move
    
    Co-authored-by: root <eredman@ualberta.ca>

--- main/Assets/Scripts/AnimalMove.cs
+++ main/Assets/Scripts/AnimalMove.cs
@@ -17,11 +17,17 @@ namespace Foundry
         //private float xDirection = 1f;
         //private float zDirection = 1f;
 
+        public float poll = 1f;
+        private float timePass = 0f;
+
+        public bool isRun = true;
+
         // Start is called before the first frame update
         void Start()
         {
             //print("(" + transform.position.x + "," + transform.position.y + "," + transform.position.z + ")");
             start = transform.position;
+            poll = UnityEngine.Random.Range(0f, 6f);
         }
 
         // Update is called once per frame
@@ -32,15 +38,28 @@ namespace Foundry
 
         void loop()
         {
-            var currentDistance = Vector3.Distance(start, transform.position);
-
-            if (currentDistance > radius)
+            timePass += Time.deltaTime;
+            if (timePass > poll)
             {
-                //transform.
-                this.transform.Rotate(0.0f, UnityEngine.Random.Range(30, 120), 0.0f);
+                isRun = !isRun;
+                timePass = 0;
+                poll = UnityEngine.Random.Range(1f, 6f);
             }
-            transform.position += transform.forward * movingSpeed * Time.deltaTime;
-            //transform.position = new Vector3(xPos, yPos, zPos);
+            if (isRun)
+            {  
+                var currentDistance = Vector3.Distance(start, transform.position);
+
+                if (currentDistance > radius)
+                {
+                    //transform.
+                    this.transform.Rotate(0.0f, UnityEngine.Random.Range(30, 120), 0.0f);
+
+                }
+                transform.position += transform.forward * movingSpeed * Time.deltaTime;
+                //transform.position = new Vector3(xPos, yPos, zPos);
+            } else {
+                
+            }
         }
     }
 }
---
+++ main/Assets/_scripts/StaffLaser.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Foundry
+{
+    public class StaffLaser : MonoBehaviour
+    {
+        public InputAction inputAction;
+        public float activationValue;
+        public Transform firingPoint;
+        public PlayerMindfullness mindfullness;
+
+        void Start()
+        {
+            // SHOOT LASERS!
+            inputAction.Enable();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            activationValue = inputAction.ReadValue<float>();
+
+            if (activationValue > 0.5) { Fire(); }
+        }
+
+        public void Fire()
+        {
+            Debug.Log("FIRE!");
+            var ray = new Ray(transform.position, firingPoint.up);
+            RaycastHit hitData;
+            if (Physics.Raycast(ray, out hitData))
+            {
+                // 80 X // 0.2 Z
+                hitData.transform.GetComponent<AnimalStats>().OnAnimalHit(mindfullness.mindfullness);
+
+            }
+        }
+    }
+}
